cc1263bdddb59380af70911dc2d0c81e7aacbf78
Rohit Pingale <pingale.mahendra@mitaoe.ac.in>
2023-09-26 17:51:51 +0530

    Updated Code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 import cvzone
-from cvzone.HandTrackingModule import HandDetection
+from cvzone.HandTrackingModule import HandDetector
 
 # Opening up the integrated camera in the local system
 capture = cv2.VideoCapture(0)
@@ -10,12 +10,12 @@ capture.set(4, 720)
 
 # Intializing hand detection using cvzone library
 # "detection confidence" and it's typically a threshold or confidence level for detecting hands. In this case, it's set to 0.8, meaning that the detector should only consider a hand as detected if it's at least 80% confident that a hand is present in the analyzed image or frame.
-detector = HandDetection(detectionConnection=0.8)
+detector = HandDetector(detectionCon=0.8)
 
 while True:
     success, image = capture.read()
     immage = detector.findHands(image)  # finds the hand in the frame
-    lmlist, bboxInfo = detector.findPositions(
+    lmlist, bboxInfo = detector.findPosition(
         image
     )  # helps finds landmarks for the hand in the frame, lmlist - landmark list, bboxInfo - bounding box
     cv2.imshow("Camera", image)
